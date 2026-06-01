62551322a9c1184c13ef3078bf1f6f7322640f6c
DROOdotFOO <drew@axol.io>
2026-06-01 04:53:21 +0200

    Drop create_session_1 from session.h

--- c_src/session.h
+++ c_src/session.h
@@ -3,7 +3,6 @@
 
 #include <erl_nif.h>
 
-ERL_NIF_TERM create_session_1(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]);
 ERL_NIF_TERM create_session_2(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]);
 ERL_NIF_TERM process_pre_key_bundle(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]);
 ERL_NIF_TERM encrypt_message(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]);
